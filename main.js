d852ef01d410199e44b07cf6e8640440c9f36d01
Филиппович Александр Александрович <filippovich.aa@corp.ertelecom.loc>
2024-08-25 18:43:42 +0300

    main js стили tapDown tapUp

--- main.js
+++ main.js
@@ -13,25 +13,70 @@
 //   })
 // })
 
-function handleColorChange(className, downColor, upColor) {
+function handleColorChange(className, downColor, upColor, childDown, childUp) {
   const children = document.querySelectorAll(`.${className}`)
   children.forEach((child) => {
-    child.addEventListener("mousedown", function () {
+    child.addEventListener("touchstart", function () {
       this.parentElement.style.background = downColor
+      child.classList.remove(childUp)
+      child.classList.add(childDown)
     })
-    child.addEventListener("mouseup", function () {
+    child.addEventListener("touchend", function () {
       this.parentElement.style.backgroundImage = `linear-gradient(
           180deg,
           rgba(0, 0, 0, 0.6) 0%,
           ${upColor} 100%
         )`
+      this.parentElement.style.background = `linear-gradient(
+        180deg,
+        rgba(0, 0, 0, 0.6) 0%,
+        ${upColor} 100%
+      )`
+      child.classList.remove(childDown)
+      child.classList.add(childUp)
     })
   })
 }
 
-handleColorChange("active-light-blue", "#0092e4", "rgba(0, 160, 228, 1)")
-handleColorChange("active-dark-blue", "#1E5DFF", "rgba(0, 160, 228, 1)")
-handleColorChange("active-pink", "#ff3f9b", "rgba(0, 160, 228, 1)")
-handleColorChange("active-purple", "#A038F1", "rgba(0, 160, 228, 1)")
-handleColorChange("active-avocado-green", "#7fe409", "rgba(0, 160, 228, 1)")
-handleColorChange("active-grass-green", "#00e577", "rgba(0, 229, 119, 1)")
+handleColorChange(
+  "active-light-blue",
+  "#0092e4",
+  "rgba(0, 160, 228, 1)",
+  "active-light-blue-child-down",
+  "active-light-blue-child-up"
+)
+handleColorChange(
+  "active-dark-blue",
+  "#1E5DFF",
+  "rgba(0, 160, 228, 1)",
+  "active-dark-blue-child-down",
+  "active-dark-blue-child-up"
+)
+handleColorChange(
+  "active-pink",
+  "#ff3f9b",
+  "rgba(0, 160, 228, 1)",
+  "active-pink-child-down",
+  "active-pink-child-up"
+)
+handleColorChange(
+  "active-purple",
+  "#A038F1",
+  "rgba(0, 160, 228, 1)",
+  "active-purple-child-down",
+  "active-purple-child-up"
+)
+handleColorChange(
+  "active-avocado-green",
+  "#7fe409",
+  "rgba(0, 160, 228, 1)",
+  "active-avocado-green-child-down",
+  "active-avocado-green-child-up"
+)
+handleColorChange(
+  "active-grass-green",
+  "#00e577",
+  "rgba(0, 229, 119, 1)",
+  "active-grass-green-child-down",
+  "active-grass-child-child-up"
+)
